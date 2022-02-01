94a46a78a5f7e5ac4d423c0c7a3428d707024672
sruly24 <srulybrach@gmail.com>
2022-02-01 14:35:24 -0500

    changed resume from pdf rendering to much simpler png

--- pages/resume.js
+++ pages/resume.js
@@ -1,40 +1,9 @@
-import React, { useState } from 'react';
-import { Document, Page,pdfjs } from 'react-pdf';
-import Footer from "@components/Footer";
-//import './pdf.css'
-
-//PDFjs worker from an external cdn
-const url =
-    "resume.pdf"
-
-export default function Test() {
-
-    pdfjs.GlobalWorkerOptions.workerSrc =
-        `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
-    const [numPages, setNumPages] = useState(null);
-    const [pageNumber, setPageNumber] = useState(1);
-
-    function onDocumentLoadSuccess({ numPages }) {
-        setNumPages(numPages);
-        setPageNumber(1);
-    }
+import Footer from '@components/Footer'
+export default function Resume() {
     return (
         <>
-            <head>
-                <title>Israel Brach</title>
-                <link rel="icon" href="/favicon.ico" />
-            </head>
-
-            <div className="pdf">
-                <Document
-                    file={url}
-                    onLoadSuccess={onDocumentLoadSuccess}
-                >
-                    <Page pageNumber={pageNumber} />
-                </Document>
-            </div>
+            <img src="resume-1.png" id={"pdf"}/>
             <Footer />
         </>
-
     );
 }
