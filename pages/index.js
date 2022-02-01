5daf305869e45ffa70aea63397a70fbedbb5e829
sruly24 <srulybrach@gmail.com>
2022-02-01 14:35:05 -0500

    updated main page

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,14 @@
-import Head from 'next/head'
-import Header from '@components/Header'
 import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <div className="container">
-      <Head/>
-      <main>
+    <>
+      <div id={"main"}>
           <img src="me.png" id="picture"/>
-        <h1 title="Welcome to my resume!" />
-        <p className="description">
-          please excuse the site while it is under construction
-        </p>
-          <a
-              href="/resume.pdf"
-              alt="Israel Brach's Resume"
-              target="_blank"
-              rel="noopener noreferrer"
-          >Download My Resume Here</a>
-      </main>
+          <h1>Hey there! Thanks for checking out my site.</h1>
+      </div>
+      <Footer/>
+    </>
 
-      <Footer />
-    </div>
   )
 }
