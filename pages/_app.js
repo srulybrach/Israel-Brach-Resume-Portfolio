40ae5479173039a16309c718e38b3917a1b45807
sruly24 <srulybrach@gmail.com>
2022-02-01 12:58:36 -0500

    added bootstrap capability

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import '@styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Head from "components/Header"
 
 function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
+        <Component {...pageProps} />
+      </>
+  );
 }
 
 export default Application
