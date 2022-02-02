9381a808772106e6addf9ded3069ad4d311f9183
sruly24 <srulybrach@gmail.com>
2022-02-02 14:16:01 -0500

    Changed sourcing

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ import { FooterText, FooterWrapper } from './FooterStyles';
 const Footer = () => {
   return (
     <FooterWrapper>
-        <FooterText>Thank you to <a href={"https://github.com/leon-luna-ray/nextjs-portfolio"}>Ray Luna </a>for the template.</FooterText>
+        <FooterText>Thank you to <a href={"https://github.com/leon-luna-ray/"}>Ray Luna </a>for the template!</FooterText>
     </FooterWrapper>
   );
 };
