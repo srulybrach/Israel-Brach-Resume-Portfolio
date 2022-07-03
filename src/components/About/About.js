c23fb5b8f2ff0e85b09a88b157ff70cd7231c103
srulybrach <srulybrach@gmail.com>
2022-07-03 14:50:31 -0400

    updated About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -17,17 +17,17 @@ const About = () => {
       <SectionText>
         {/* Personal profile image */}
         <Img src='/me.png' alt="Israel Brach's profile picture" />
-        Hi, I'm Israel! I am a student studying software engineering at
-        Touro College in New York. I am currently in my final semester of
+        Hi, I'm Israel! I am a graduate of software engineering at
+        Touro College in New York. I just finished my final semester of
         Computer Science, and can't wait to get out there and start coding!
       </SectionText>
       <SectionText>
-        I got into coding because I enjoy problem solving. The rush of having
+        I got into coding in 2014 because I enjoy problem solving. The rush of having
         a computer correctly understand my solution to a problem is rarely met.
       </SectionText>
       <SectionText>
         I live in New York City. My hobbies are 3D Printing,
-        growing crops, playing video games, cooking, general tinkering, travel and more.
+        growing crops, playing video games, cooking, general tinkering, traveling and more.
       </SectionText>
       <br />
       <br />
