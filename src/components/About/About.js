63e3eb5790c21bf3779513f6d6eecbc50c2459b4
srulybrach <srulybrach@gmail.com>
2023-05-16 22:10:17 -0400

    update

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -19,7 +19,7 @@ const About = () => {
         <Img src='/me.png' alt="Israel Brach's profile picture" />
           Hello, my name is Israel! I work as a Technical Project Manager at B&H Photo Video,
           where I've worked since completing my education. I earned my Bachelor's degree
-          in Software Engineering from Touro College in New York, graduating in 2022.
+          in Software Engineering from Touro University in New York, graduating in 2022.
       </SectionText>
       <SectionText>
         I got into coding in 2014 because I enjoy problem solving. The rush of having
@@ -27,7 +27,7 @@ const About = () => {
         role as a project manager to work with many teams, being part of so much problem solving.
       </SectionText>
       <SectionText>
-        I live in New York City. Some of my hobbies are 3D Printing, playing video games, cooking, general tinkering, and traveling.
+        I live in New York City. Some of my hobbies include 3D Printing, playing video games, cooking, general tinkering, and traveling.
       </SectionText>
       <br />
       <br />
